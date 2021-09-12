34b54aedbfb3b0644f6a7c55d61c63757d5787f6
JacobShiber <yaakovshiber@gmail.com>
2021-09-12 17:52:40 +0300

    minor fixes

--- main.js
+++ main.js
@@ -8,15 +8,30 @@ categoriesLink.onclick = () => {
 
     let slidingCat = setInterval(() => {
         catLinksContainer.style = `height: ${counter++}px; width: 95%; margin-left: 2.5%; background-color: rgb(157, 0, 0);`;
+
         if (counter == 70) {
-        clearInterval(slidingCat);
-    }
+            clearInterval(slidingCat);
+        }
     }, 5);
-    
+
     setTimeout(() => {
-       for (let i = 0; i < categoriesLinks.length; i++) {
-        catLinks.innerHTML += `<li class = "catLink"><a href = "">${categoriesLinks[i].toUpperCase()}</a></li>`
-    } 
-    }, 300);
-    
+        for (let i = 0; i < categoriesLinks.length; i++) {
+            catLinks.innerHTML += `<li class = "catLink"><a href = "">${categoriesLinks[i].toUpperCase()}</a></li>`;
+        }
+    }, 500);
+
+};
+
+catLinksContainer.onclick = () => {
+    let counter = 70;
+
+    let slidingCatOut = setInterval(() => {
+        catLinksContainer.style = `height: ${counter--}px; width: 95%; margin-left: 2.5%; background-color: rgb(157, 0, 0);`;
+        if (counter == 0) {
+            clearInterval(slidingCatOut)
+        }
+    }, 5);
+    setTimeout(() => {
+        catLinks.innerHTML = "";
+    }, 100);
 };
